0ff7a70febf093fb88f946d564bd9785a15d5bbf
yanwenboo <greatgodyan@163.com>
2020-10-20 10:46:25 +0800

    修改test service 测试类

--- letterball_user/src/main/java/com/letterball/controller/JsonJX.java
+++ letterball_user/src/main/java/com/letterball/controller/JsonJX.java
@@ -11,6 +11,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * json解析s
+ */
 @RequestMapping("/jsonData")
 @RestController
 public class JsonJX extends BaseService {
@@ -19,7 +28,7 @@ public class JsonJX extends BaseService {
      * json解析
      */
     @PostMapping("/jsonData")
-    public ResponseBase jsonData(@RequestBody String jsonData) throws JSONException {
+    public ResponseBase jsonData(@RequestBody String jsonData) throws JSONException, IOException {
 
         /**
          * 方法解析
@@ -27,9 +36,10 @@ public class JsonJX extends BaseService {
         //     JSONObject jsonObject   = JSON.parseObject(json字符串)
         //   {}  =  data = jsonObject.getJSONObject("data");
         //   []  =  jsonObject.getJSONObject("data");
+        String jsonData1 = readJsonFile();
 
 
-        JSONObject jsonObject = JSON.parseObject(jsonData);
+        JSONObject jsonObject = JSON.parseObject(jsonData1);
 //        String data = jsonObject.getString("data");
         JSONObject data = jsonObject.getJSONObject("data");
         System.err.println("总数据" + data);
@@ -56,9 +66,44 @@ public class JsonJX extends BaseService {
             System.err.println(name + id);
         }
 
+        return setResultSuccessData(jsonData1);
+    }
 
 
-        return setResultSuccessData(jsonData);
+    //读取json文件
+    @RequestMapping("/redsJson")
+    public String readJsonFile() throws IOException {
+        String jsonStr = "";
+        try  {
+            String path = JsonJX.class.getClassLoader().getResource("jsonData.json").getPath();
+            URI uri = JsonJX.class.getClassLoader().getResource("jsonData.json").toURI();
+
+            FileInputStream jsonFile = new FileInputStream(new File(path));
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+            FileChannel channel = jsonFile.getChannel();
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+
+            StringBuffer sb = new StringBuffer();
+
+//            List<String> strings = Files.readAllLines(Paths.get(uri));
+//            strings.stream().forEach(sb::append);
+
+
+
+            while(channel.read(buffer) != -1){
+                buffer.flip();
+                out.write(buffer.array());
+                buffer.clear();
+            }
+            channel.close();
+            jsonStr = sb.append(new String(out.toByteArray())).toString();
+            return jsonStr;
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 
